bde3b6126d4742a53c42aa049a09be7d51016e0f
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-04-19 09:43:01 +0700

    Update training.py

--- training.py
+++ training.py
@@ -3,13 +3,13 @@ from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
 os.chdir('../training')
-import shutil
-shutil.make_archive('HEINZO', 'zip', '/content/images')
+#import shutil
+#shutil.make_archive('HEINZO', 'zip', '/content/images')
 full_local_path = "/content/training"
 
-backupname = "/content/training/HEINZO.zip"
-backup = ('/content/training/H' + str(tgl) + '.zip')
-os.rename(backupname, backup)
+#backupname = "/content/training/HEINZO.zip"
+#backup = ('/content/training/H' + str(tgl) + '.zip')
+#os.rename(backupname, backup)
 
 
 repo = Repo(full_local_path)
